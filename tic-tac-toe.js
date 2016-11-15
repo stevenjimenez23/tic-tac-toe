14337d1fb162a842caa46fb14d2e5d1c012ecd76
steven w jimenez <stevenjimenez724@gmail.com>
2016-11-15 10:16:16 -0800

    almst finished

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,17 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function shape(){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx","50")
+  circle.setAttribute("cy","70")
+  circle.setAttribute("r","20")
+circle.setAttribute("fill","red")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
+function xxx(){
+  var rect = document.createElementNS(namespace, "rectangle")
+rect.setAttribute()
+  canvas.appendChild(line)
+}
