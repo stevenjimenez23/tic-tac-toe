d7a22627dde88060b926d88853366b4ca2d69650
steven w jimenez <stevenjimenez724@gmail.com>
2016-11-16 10:18:12 -0800

    did some pretty tubular sttuff

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,24 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var message = "sup"
 function shape(){
-  var circle = document.createElementNS(namespace, "circle")
-  circle.setAttribute("cx","50")
-  circle.setAttribute("cy","70")
-  circle.setAttribute("r","20")
-circle.setAttribute("fill","red")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
+  if(message == "sup"){
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx","50")
+    circle.setAttribute("cy","70")
+    circle.setAttribute("r","20")
+  circle.setAttribute("fill","red")
+
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+  message = "peace"
 }
-function xxx(){
-  var rect = document.createElementNS(namespace, "rectangle")
-rect.setAttribute()
-  canvas.appendChild(line)
+
+}
+var boi = "boii"
+function  middle(){
+  if(boi == "boii"){
+    var cir
+  }
 }
